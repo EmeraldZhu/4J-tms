5040ff9ab6739dc82a643fb7c2d23dafc5e7e976
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 01:19:35 +0300

    create firebase config file

---
+++ src/services/firebase-config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAyK6EtnNM-YqHDaktclq9RulNSjoYehT8",
+  authDomain: "four-j-tms.firebaseapp.com",
+  projectId: "four-j-tms",
+  storageBucket: "four-j-tms.appspot.com",
+  messagingSenderId: "149128844884",
+  appId: "1:149128844884:web:903900f87a059a173affc1"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
