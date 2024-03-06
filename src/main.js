6221ed893c69bf86f6467aa1d8d443ddd027dcb1
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 01:23:57 +0300

    Changed Vue app to use PrimeVue library

--- src/main.js
+++ src/main.js
@@ -1,5 +1,12 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
+import { createApp } from 'vue';
+import App from './App.vue';
+import PrimeVue from 'primevue/config';
+import 'primevue/resources/themes/saga-blue/theme.css' // Theme
+import 'primevue/resources/primevue.min.css' // Core CSS
+import 'primeicons/primeicons.css' // Icons
 
-createApp(App).mount('#app')
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+app.mount('#app');
