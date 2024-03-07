6e0c0f643af1886a12bff3ee187ee61d7ecb2815
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 09:17:34 +0300

    Add RegisterOwner route

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,18 @@
 // src/router/index.js
 import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
+import RegisterOwner from '../views/RegisterOwner.vue';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
     component: HomePage,
+  },
+  {
+    path: '/register/owner',
+    name: 'RegisterOwner',
+    component: RegisterOwner,
   },
   // ... your other routes
 ];
