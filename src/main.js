c920477d1a2745fd7e7ecef21088c28bf893f11b
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 17:03:13 +0300

    Added PrimeFlex and Toast CSS to the project

--- src/main.js
+++ src/main.js
@@ -4,15 +4,19 @@ import firebaseConfig from './services/firebase-config';
 import router from './router';
 import { store } from './store';
 import PrimeVue from 'primevue/config';
+import ToastService from 'primevue/toastservice';
 
 import 'primevue/resources/themes/saga-blue/theme.css' // Theme
 import 'primevue/resources/primevue.min.css' // Core CSS
 import 'primeicons/primeicons.css' // Icons
+import 'primevue/resources/primeflex/primeflex.css'; // If you use PrimeFlex
+import 'primevue/components/toast/toast.css';
 
 const app = createApp(App);
 
 app.use(PrimeVue);
 app.use(store);
 app.use(router);
+app.use(ToastService);
 
 app.mount('#app');
