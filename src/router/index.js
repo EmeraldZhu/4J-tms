b670e54694f1758f07b1a5899fb886c72f7d75db
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 15:10:09 +0300

    Added ResetPassword route for owners

--- src/router/index.js
+++ src/router/index.js
@@ -3,6 +3,7 @@ import { createRouter, createWebHistory } from 'vue-router';
 import HomePage from '../views/HomePage.vue';
 import RegisterOwner from '../views/auth/RegisterOwner.vue';
 import LoginOwner from '../views/auth/LoginOwner.vue';
+import ResetPassword from '../views/auth/ResetPassword.vue';
 
 const routes = [
   {
@@ -19,7 +20,12 @@ const routes = [
     path: '/login/owner',
     name: 'LoginOwner',
     component: LoginOwner,
-  }
+  },
+  {
+    path: '/reset-password/owner',
+    name: 'ResetPassword',
+    component: ResetPassword,
+  },
   // ... your other routes
 ];
 
