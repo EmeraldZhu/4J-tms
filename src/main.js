cbca0e6b4f6b0638bad46558fe6e9c1ea1cd3b2a
Emerald Zhu <zhuemerald@gmail.com>
2024-03-07 10:30:02 +0300

    Added Vuex store to the Vue application

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import { createApp } from 'vue';
 import App from './App.vue';
 import router from './router';
+import store from './store';
 import firebaseConfig from './services/firebase-config';
 import PrimeVue from 'primevue/config';
 
@@ -11,6 +12,7 @@ import 'primeicons/primeicons.css' // Icons
 const app = createApp(App);
 
 app.use(PrimeVue);
+app.use(store);
 app.use(router);
 
 app.mount('#app');
