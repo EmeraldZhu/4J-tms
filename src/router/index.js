c88811f3a2405d6eea49ebea99bd875c96dfd1ab
Emerald Zhu <zhuemerald@gmail.com>
2024-03-11 15:09:52 +0300

    Add owner dashboard and nested routes for owner's pages

--- src/router/index.js
+++ src/router/index.js
@@ -4,6 +4,12 @@ import HomePage from '../views/HomePage.vue';
 import RegisterOwner from '../views/auth/RegisterOwner.vue';
 import LoginOwner from '../views/auth/LoginOwner.vue';
 import ResetPassword from '../views/auth/ResetPassword.vue';
+import OwnerDashboard from '../components/layouts/OwnerDashboard.vue';
+import Dashboard from '../views/owner/Dashboard.vue';
+import Maintenance from '../views/owner/Maintenance.vue';
+import NoticeBoard from '../views/owner/NoticeBoard.vue';
+import Properties from '../views/owner/Properties.vue';
+import Tenants from '../views/owner/Tenants.vue';
 
 const routes = [
   {
@@ -26,11 +32,43 @@ const routes = [
     name: 'ResetPassword',
     component: ResetPassword,
   },
+  {
+    path: '/owner',
+    component: OwnerDashboard,
+    children: [
+      {
+        path: '',
+        name: 'OwnerProperties',
+        component: Properties,
+      },
+      {
+        path: 'dashboard',
+        name: 'OwnerDashboard',
+        component: Dashboard,
+      },
+      {
+        path: 'maintenance',
+        name: 'OwnerMaintenance',
+        component: Maintenance,
+      },
+      {
+        path: 'notice-board',
+        name: 'OwnerNoticeBoard',
+        component: NoticeBoard,
+      },
+      {
+        path: 'tenants',
+        name: 'OwnerTenants',
+        component: Tenants,
+      },
+    ]
+  },
+
   // ... your other routes
 ];
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(process.env.BASE_URL),
   routes,
 });
 
