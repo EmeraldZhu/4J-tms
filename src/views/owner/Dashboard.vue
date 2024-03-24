6484e0ff66dc17a6ad6f2d4b70869e4afce39c3a
Emerald Zhu <zhuemerald@gmail.com>
2024-03-25 02:16:39 +0300

    Add OverlayPanel for property and unit editing

--- src/views/owner/Dashboard.vue
+++ src/views/owner/Dashboard.vue
@@ -1,52 +1,107 @@
 <template>
-    <div class="grid">
-        <Toast />
-        <ConfirmPopup></ConfirmPopup>
-        <div class="col-6 border-round">
-            <h3>Your Properties</h3>
-            <div class="properties-datatable">
-                <DataTable :value="properties">
-                <!-- Define your columns here -->
-                        <Column field="media" header="Image">
-                                <template #body="slotProps">
-                                        <img :src="slotProps.data.media" alt="Property Image" style="width: 50px; height: 50px;">
-                                </template>
-                        </Column>
-                        <Column field="name" header="Property"></Column>
-                        <Column field="units" header="Units"></Column>
-                        <Column field="address" header="Address"></Column>
-                        <Column header="Action">
-                                <template #body="slotProps">
-                                        <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2" @click="editProperty(slotProps.data)" />
-                                        <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="deleteProperty(slotProps.data, $event)" />
-                                </template>
-                        </Column>
-                </DataTable>
-            </div>
+  <div class="grid">
+    <Toast />
+    <ConfirmPopup></ConfirmPopup>
+    <OverlayPanel ref="op" :showCloseIcon="true" class="overlaypanel-demo" appendTo="body">
+      <form @submit.prevent="updateProperty" class="p-fluid">
+        <div class="p-field">
+          <label for="name">Name</label>
+          <InputText id="name" v-model="selectedProperty.name" placeholder="Name" />
         </div>
-        <div class="col-6 border-round">
-            <h3>Your Units</h3>
-            <div class="property-selector">
-                <Dropdown v-model="selectedProperty" :options="landlordProperties" optionLabel="label" placeholder="Select a property" />
-            </div>
-            <br>
-            <div class="units-datatable">
-                <DataTable :value="units">
-                <!-- Define your columns here -->
-                        <Column field="propertyName.label" header="Property Name"></Column>
-                        <Column field="propertyUnitName.label" header="Unit Name"></Column>
-                        <Column field="unitType.label" header="Unit Type"></Column>
-                        <Column field="rentPrice" header="Rent"></Column>
-                        <Column header="Action">
-                                <template #body="slotProps">
-                                        <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2" @click="editUnit(slotProps.data)" />
-                                        <Button icon="pi pi-trash" class="p-button-rounded p-button-danger" @click="deleteUnit(slotProps.data, $event)" />
-                                </template>
-                        </Column>
-                </DataTable>
-            </div> 
+        <div class="p-field">
+          <label for="address">Address</label>
+          <InputText id="address" v-model="selectedProperty.address" placeholder="Address" />
         </div>
+        <div class="p-field">
+          <label for="units">Units</label>
+          <InputText id="units" v-model="selectedProperty.units" placeholder="Units" type="number" />
+        </div>
+        <div class="p-field">
+          <label for="unitNames">Unit Names</label>
+          <InputText id="unitNames" v-model="selectedProperty.unitNames" placeholder="Unit Names" />
+        </div>
+        <div class="p-field">
+          <label for="media">Image URL</label>
+          <InputText id="media" v-model="selectedProperty.media" placeholder="Image URL" />
+        </div>
+        <br>
+        <Button label="Update Property" type="submit" class="p-mt-2" />
+      </form>
+    </OverlayPanel>
+    <div class="col-6 border-round">
+      <h3>Your Properties</h3>
+      <div class="properties-datatable">
+        <DataTable :value="properties">
+          <!-- Define your columns here -->
+          <Column field="media" header="Image">
+            <template #body="slotProps">
+              <img :src="slotProps.data.media" alt="Property Image" style="width: 50px; height: 50px;">
+            </template>
+          </Column>
+          <Column field="name" header="Property"></Column>
+          <Column field="units" header="Units"></Column>
+          <Column field="address" header="Address"></Column>
+          <Column header="Action">
+            <template #body="slotProps">
+              <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2"
+                @click="openEditPanel(slotProps.data, $event)" />
+              <Button icon="pi pi-trash" class="p-button-rounded p-button-danger"
+                @click="deleteProperty(slotProps.data, $event)" />
+            </template>
+          </Column>
+        </DataTable>
+      </div>
     </div>
+    <OverlayPanel ref="opUnit" class="overlaypanel-demo">
+      <form @submit.prevent="updateUnit" class="p-fluid">
+        <div class="p-field">
+          <label for="propertyName">Property Name</label>
+          <Dropdown id="propertyName" v-model="selectedUnit.propertyName.value" :options="landlordProperties"
+            optionLabel="label" placeholder="Select a property" />
+        </div>
+        <div class="p-field">
+          <label for="propertyUnitName">Unit Name</label>
+          <InputText id="propertyUnitName" v-model="selectedUnit.propertyUnitName.label" placeholder="Unit Name" />
+        </div>
+        <div class="p-field">
+          <label for="unitType">Unit Type</label>
+          <Dropdown id="unitType" v-model="selectedUnit.unitType.value" :options="unitTypes" optionLabel="label"
+            placeholder="Select unit type" />
+        </div>
+        <div class="p-field">
+          <label for="rentPrice">Rent Price</label>
+          <InputText id="rentPrice" v-model="selectedUnit.rentPrice" placeholder="Rent Price" type="number" />
+        </div>
+        <br>
+        <Button label="Update Unit" type="submit" class="p-mt-2" />
+      </form>
+    </OverlayPanel>
+    <div class="col-6 border-round">
+      <h3>Your Units</h3>
+      <div class="property-selector">
+        <Dropdown v-model="selectedProperty" :options="landlordProperties" optionLabel="label"
+          placeholder="Select a property" />
+      </div>
+      <br>
+      <div class="units-datatable">
+        <DataTable :value="units">
+          <!-- Define your columns here -->
+          <Column field="propertyName.label" header="Property Name"></Column>
+          <Column field="propertyUnitName.label" header="Unit Name"></Column>
+          <Column field="unitType.label" header="Unit Type"></Column>
+          <Column field="rentPrice" header="Rent"></Column>
+          <Column header="Action">
+            <template #body="slotProps">
+              <Button icon="pi pi-pencil" class="p-button-rounded p-button-success mr-2"
+                @click="openEditUnitPanel(slotProps.data, $event)" />
+              <Button icon="pi pi-trash" class="p-button-rounded p-button-danger"
+                @click="deleteUnit(slotProps.data, $event)" />
+            </template>
+          </Column>
+        </DataTable>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script setup>
@@ -55,10 +110,12 @@ import Toast from 'primevue/toast';
 import { useToast } from 'primevue/usetoast';
 import ConfirmPopup from 'primevue/confirmpopup';
 import { useConfirm } from 'primevue/useconfirm';
+import OverlayPanel from 'primevue/overlaypanel';
 import DataTable from 'primevue/datatable';
 import Column from 'primevue/column';
 import Dropdown from 'primevue/dropdown';
 import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
 import { getAuth } from 'firebase/auth';
 import { getFirestore, collection, query, where, getDocs, addDoc, deleteDoc, updateDoc, doc } from 'firebase/firestore';
 
@@ -71,6 +128,10 @@ const selectedProperty = ref(null);
 const landlordProperties = ref([]);
 const toast = useToast();
 const confirm = useConfirm();
+const op = ref(null);
+const opUnit = ref(null);
+const selectedUnit = ref({});
+const unitTypes = ref([]);
 
 const fetchProperties = async () => {
   if (!user) return;
@@ -108,9 +169,9 @@ const deleteProperty = async (property) => {
       try {
         await deleteDoc(doc(db, 'properties', property.id));
         properties.value = properties.value.filter(p => p.id !== property.id);
-        toast.add({severity: 'success', summary: 'Delete Successful', detail: `Property "${property.name}" has been deleted.`, life: 3000});
+        toast.add({ severity: 'success', summary: 'Delete Successful', detail: `Property "${property.name}" has been deleted.`, life: 3000 });
       } catch (error) {
-        toast.add({severity: 'error', summary: 'Delete Failed', detail: 'Failed to delete property.', life: 3000});
+        toast.add({ severity: 'error', summary: 'Delete Failed', detail: 'Failed to delete property.', life: 3000 });
       }
     }
   });
@@ -128,11 +189,68 @@ const deleteUnit = async (unit) => {
       try {
         await deleteDoc(doc(db, 'units', unit.id));
         units.value = units.value.filter(u => u.id !== unit.id);
-        toast.add({severity: 'success', summary: 'Delete Successful', detail: `Unit "${unit.propertyUnitName.label}" has been deleted.`, life: 3000});
+        toast.add({ severity: 'success', summary: 'Delete Successful', detail: `Unit "${unit.propertyUnitName.label}" has been deleted.`, life: 3000 });
       } catch (error) {
-        toast.add({severity: 'error', summary: 'Delete Failed', detail: 'Failed to delete unit.', life: 3000});
+        toast.add({ severity: 'error', summary: 'Delete Failed', detail: 'Failed to delete unit.', life: 3000 });
       }
     }
   });
 };
+
+const openEditPanel = (property, event) => {
+  selectedProperty.value = { ...property };
+  op.value.toggle(event);
+};
+
+const updateProperty = async () => {
+  try {
+    await updateDoc(doc(db, 'properties', selectedProperty.value.id), selectedProperty.value);
+    toast.add({ severity: 'success', summary: 'Success', detail: 'Property updated successfully.', life: 3000 });
+    fetchProperties(); // Refresh the properties list
+    op.value.hide(); // Close the OverlayPanel
+  } catch (error) {
+    console.error("Error updating property: ", error);
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to update property.', life: 3000 });
+  }
+};
+
+const openEditUnitPanel = (unit, event) => {
+  selectedUnit.value = { ...unit };
+  opUnit.value.toggle(event);
+};
+
+const fetchUnitTypes = async () => {
+  const querySnapshot = await getDocs(collection(db, 'unitTypes'));
+  unitTypes.value = querySnapshot.docs.map(doc => ({ label: doc.data().type, value: doc.id }));
+};
+
+const updateUnit = async () => {
+  try {
+    const updatedUnit = {
+      propertyName: { label: landlordProperties.value.find(p => p.value === selectedUnit.value.propertyName.value).label, value: selectedUnit.value.propertyName.value },
+      propertyUnitName: {
+        label: selectedUnit.value.propertyUnitName.label, // New unit name from form
+        value: selectedUnit.value.propertyUnitName.label // Use the same new name for both label and value
+      },
+      unitType: { label: unitTypes.value.find(ut => ut.value === selectedUnit.value.unitType.value).label, value: selectedUnit.value.unitType.value },
+      rentPrice: selectedUnit.value.rentPrice
+    };
+    await updateDoc(doc(db, 'units', selectedUnit.value.id), updatedUnit);
+      // propertyName: { label: landlordProperties.value.find(p => p.value === selectedUnit.value.propertyName.value).label, value: selectedUnit.value.propertyName.value },
+      // propertyUnitName: { ...selectedUnit.value.propertyUnitName },
+      // unitType: { label: unitTypes.value.find(ut => ut.value === selectedUnit.value.unitType.value).label, value: selectedUnit.value.unitType.value },
+      // rentPrice: selectedUnit.value.rentPrice
+    toast.add({ severity: 'success', summary: 'Success', detail: 'Unit updated successfully.', life: 3000 });
+    fetchUnits(selectedProperty.value);
+    opUnit.value.hide(); // Close the OverlayPanel
+  } catch (error) {
+    console.error("Error updating unit: ", error);
+    toast.add({ severity: 'error', summary: 'Error', detail: 'Failed to update unit.', life: 3000 });
+  }
+};
+
+onMounted(() => {
+  fetchUnitTypes();
+  fetchProperties();
+});
 </script>
