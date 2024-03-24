3d2ba729f6b204d3199eeb64219bb6d7bc7890e9
Emerald Zhu <zhuemerald@gmail.com>
2024-03-24 11:30:53 +0300

    create boilerplate DataTables for properties and units

--- src/views/owner/Dashboard.vue
+++ src/views/owner/Dashboard.vue
@@ -1,9 +1,28 @@
 <template>
-    
-        Dashboard here
-    
+    <div class="grid">
+        <div class="col-6 border-round">
+            <h3>Your Properties</h3>
+            <div class="properties-datatable">
+                <DataTable :value="properties">
+                <!-- Define your columns here -->
+                </DataTable>
+            </div>
+        </div>
+        <div class="col-6 border-round">
+            <h3>Your Units</h3>
+            <div class="property-selector">
+                <Dropdown v-model="selectedProperty" :options="landlordProperties" optionLabel="label" placeholder="Select a property" />
+            </div>
+            <div class="units-datatable">
+                <DataTable :value="units">
+                <!-- Define your columns here -->
+                </DataTable>
+            </div> 
+        </div>
+    </div>
 </template>
 
 <script setup>
-
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 </script>
